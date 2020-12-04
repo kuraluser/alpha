7f25a9933a184bbbe4de689fdd1387890ce8fc39
binilshkb <30854446+binilshkb@users.noreply.github.com>
2020-12-04 18:53:16 +0530

    CPDSS  410:Confirmation Dialogue Box (#116)
    * dss 410 error massage toast design
    
    * design changes
    
    * Update new-voyage-popup.component.ts
    
    * Update new-voyage-popup.component.ts
    
    * confirmation dialogue box
    
    * overlay style
    
    * DSS-305:Angular-Create a confirmation dialog component for user confirmation - Toggle class for overlay
    
    * Update new-voyage-popup.component.ts
    
    Co-authored-by: binilshkb <binish.kb@alphaori.sg>
    Co-authored-by: Varsha-TS <varsha.ts@alphaori.sg>

--- web/cpdss-portal/apps/cpdss/src/app/shared/components/confirmation-alert/confirmation-alert.service.ts
+++ web/cpdss-portal/apps/cpdss/src/app/shared/components/confirmation-alert/confirmation-alert.service.ts
@@ -13,19 +13,42 @@ import { IConfirmationMessage } from './confirmation-alert.model';
 export class ConfirmationAlertService {
 
   private _confirmSource: Subject<boolean> = new Subject();
+  private _showSource: Subject<boolean> = new Subject();
+
   confirmAlert$ = this._confirmSource.asObservable();
+  showToast$ = this._showSource.asObservable();
 
   constructor(private messageService: MessageService) {
   }
 
+  /**
+   * Method for showing confirmation dialog
+   *
+   * @param {IConfirmationMessage} message
+   * @memberof ConfirmationAlertService
+   */
   add(message: IConfirmationMessage) {
-    this.messageService.add(message);
+    this._showSource.next(true);
+    this.messageService.add({ ...message, closable: false });
   }
 
+  /**
+   * Method for clearing confirmation dialog
+   *
+   * @param {string} key
+   * @memberof ConfirmationAlertService
+   */
   clear(key: string) {
+    this._showSource.next(false);
     this.messageService.clear(key);
   }
 
+  /**
+   * Method for setting user confirmation
+   *
+   * @param {boolean} isConfirmed
+   * @memberof ConfirmationAlertService
+   */
   onConfirm(isConfirmed: boolean) {
     this._confirmSource.next(isConfirmed);
   }
