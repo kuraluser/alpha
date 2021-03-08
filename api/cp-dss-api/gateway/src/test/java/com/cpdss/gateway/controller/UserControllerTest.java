65194b6f3c6941ff02495f6ee04e1275d4c9391c
johnsoorajxavier <79301295+johnsoorajxavier@users.noreply.github.com>
2021-03-08 10:27:30 +0530

    DSS-1693: Java Delete User 1. Test Cases type: feature

--- api/cp-dss-api/gateway/src/test/java/com/cpdss/gateway/controller/UserControllerTest.java
+++ api/cp-dss-api/gateway/src/test/java/com/cpdss/gateway/controller/UserControllerTest.java
@@ -7,6 +7,7 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -132,6 +133,15 @@ public class UserControllerTest {
                 .header(AUTHORIZATION_HEADER, "4b5608ff-b77b-40c6-9645-d69856d4aafa")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void deleteUserTest() throws Exception {
+    this.mockMvc
+        .perform(
+            delete("/api/ship/users/delete/{userId}", 1l)
+                .header(AUTHORIZATION_HEADER, "4b5608ff-b77b-40c6-9645-d69856d4aafa"))
         .andExpect(status().isOk());
   }
 }
