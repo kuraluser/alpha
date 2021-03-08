0f0a292000a78cf6e6a4d2841e1f4cadedf70a7f
Rishiraj CR <rishiraj.cr@Thinkpalm.com>
2021-03-08 08:57:56 +0530

    DSS-1638: unwanted comments removed and added methos comments.

--- web/cpdss-portal/apps/cpdss/src/app/business/cargo-planning/cargo-history/cargo-history.component.ts
+++ web/cpdss-portal/apps/cpdss/src/app/business/cargo-planning/cargo-history/cargo-history.component.ts
@@ -3,6 +3,13 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { IVessel } from '../../core/models/vessel-details.model';
 import { VesselsApiService } from '../../core/services/vessels-api.service';
 
+/**
+ * Component to show Cargo history
+ *
+ * @export
+ * @class CargoHistoryComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'cpdss-portal-cargo-history',
   templateUrl: './cargo-history.component.html',
@@ -21,10 +28,13 @@ export class CargoHistoryComponent implements OnInit {
     this.initMethod();
   }
 
+  /**
+   * Temporary function to show vessel info
+   * @memberof CargoHistoryComponent
+   */
   async initMethod() {
     this.ngxSpinnerService.show();
     const res = await this.vesselsApiService.getVesselsInfo().toPromise();
-    console.log(res)
     this.vesselInfo = res[0] ?? <IVessel>{};
     this.ngxSpinnerService.hide();
   }
