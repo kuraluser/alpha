7c0deed358edcb193765df8c9fbb8b27bac2fef1
johnsoorajxavier <79301295+johnsoorajxavier@users.noreply.github.com>
2021-04-20 19:50:45 +0530

    Compilation error due to similar methods 1. spotless apply

--- api/cp-dss-api/loadable-study/src/main/java/com/cpdss/loadablestudy/repository/ApiTempHistoryRepository.java
+++ api/cp-dss-api/loadable-study/src/main/java/com/cpdss/loadablestudy/repository/ApiTempHistoryRepository.java
@@ -20,7 +20,7 @@ public interface ApiTempHistoryRepository
           + "ath.cargoId = ?1 AND ath.year >= ?2 order by ath.loadedDate desc")
   public List<com.cpdss.loadablestudy.entity.ApiTempHistory> findApiTempHistoryWithYearAfter(
       Long cargoId, Integer year);
-  
+
   public List<ApiTempHistory> findByLoadingPortIdAndCargoIdAndIsActiveOrderByCreatedDateTimeDesc(
       Long loadingPortId, Long cargoId, Boolean isActive);
 
