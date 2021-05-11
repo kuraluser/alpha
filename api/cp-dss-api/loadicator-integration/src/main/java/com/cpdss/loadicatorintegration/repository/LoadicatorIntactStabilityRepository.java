5ae7590ee2914e58fc2b5281af4d6cbf37063235
pranavkrisha97 <pranav.k@alphaori.sg>
2021-05-11 05:30:14 +0530

    ordering trim, strength and intact stability based on id

--- api/cp-dss-api/loadicator-integration/src/main/java/com/cpdss/loadicatorintegration/repository/LoadicatorIntactStabilityRepository.java
+++ api/cp-dss-api/loadicator-integration/src/main/java/com/cpdss/loadicatorintegration/repository/LoadicatorIntactStabilityRepository.java
@@ -5,9 +5,12 @@ import com.cpdss.common.springdata.CommonCrudRepository;
 import com.cpdss.loadicatorintegration.entity.IntactStability;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+
 public interface LoadicatorIntactStabilityRepository
     extends CommonCrudRepository<IntactStability, Long> {
 
+  @Query("FROM IntactStability WHERE stowagePlanId in ?1 ORDER BY Id")
   List<IntactStability> findByStowagePlanIdIn(List<Long> stowagePlanIds);
 
   public IntactStability findByStowagePlanId(Long id);
