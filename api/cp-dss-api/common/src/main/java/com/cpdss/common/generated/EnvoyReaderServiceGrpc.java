788b3262dc5b5f5cb3fe429efbec08e094e62e67
Jerin George <jerin.g@thinkpalm.com>
2021-05-20 19:13:20 +0530

    ship build changes

--- api/cp-dss-api/common/src/main/java/com/cpdss/common/generated/EnvoyReaderServiceGrpc.java
+++ api/cp-dss-api/common/src/main/java/com/cpdss/common/generated/EnvoyReaderServiceGrpc.java
@@ -1,7 +1,22 @@
-/* Licensed at AlphaOri Technologies */
 package com.cpdss.common.generated;
 
-/** */
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.27.1)",
     comments = "Source: envoy_reader.proto")
@@ -12,60 +27,65 @@ public final class EnvoyReaderServiceGrpc {
   public static final String SERVICE_NAME = "EnvoyReaderService";
 
   // Static method descriptors that strictly reflect the proto.
-  /** Creates a new async stub that supports all call types for the service */
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
   public static EnvoyReaderServiceStub newStub(io.grpc.Channel channel) {
     io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceStub> factory =
-        new io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceStub>() {
-          @java.lang.Override
-          public EnvoyReaderServiceStub newStub(
-              io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
-            return new EnvoyReaderServiceStub(channel, callOptions);
-          }
-        };
+      new io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceStub>() {
+        @java.lang.Override
+        public EnvoyReaderServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new EnvoyReaderServiceStub(channel, callOptions);
+        }
+      };
     return EnvoyReaderServiceStub.newStub(factory, channel);
   }
 
   /**
    * Creates a new blocking-style stub that supports unary and streaming output calls on the service
    */
-  public static EnvoyReaderServiceBlockingStub newBlockingStub(io.grpc.Channel channel) {
+  public static EnvoyReaderServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
     io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceBlockingStub> factory =
-        new io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceBlockingStub>() {
-          @java.lang.Override
-          public EnvoyReaderServiceBlockingStub newStub(
-              io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
-            return new EnvoyReaderServiceBlockingStub(channel, callOptions);
-          }
-        };
+      new io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceBlockingStub>() {
+        @java.lang.Override
+        public EnvoyReaderServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new EnvoyReaderServiceBlockingStub(channel, callOptions);
+        }
+      };
     return EnvoyReaderServiceBlockingStub.newStub(factory, channel);
   }
 
-  /** Creates a new ListenableFuture-style stub that supports unary calls on the service */
-  public static EnvoyReaderServiceFutureStub newFutureStub(io.grpc.Channel channel) {
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static EnvoyReaderServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
     io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceFutureStub> factory =
-        new io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceFutureStub>() {
-          @java.lang.Override
-          public EnvoyReaderServiceFutureStub newStub(
-              io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
-            return new EnvoyReaderServiceFutureStub(channel, callOptions);
-          }
-        };
+      new io.grpc.stub.AbstractStub.StubFactory<EnvoyReaderServiceFutureStub>() {
+        @java.lang.Override
+        public EnvoyReaderServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new EnvoyReaderServiceFutureStub(channel, callOptions);
+        }
+      };
     return EnvoyReaderServiceFutureStub.newStub(factory, channel);
   }
 
-  /** */
-  public abstract static class EnvoyReaderServiceImplBase implements io.grpc.BindableService {
+  /**
+   */
+  public static abstract class EnvoyReaderServiceImplBase implements io.grpc.BindableService {
 
-    @java.lang.Override
-    public final io.grpc.ServerServiceDefinition bindService() {
-      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor()).build();
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .build();
     }
   }
 
-  /** */
-  public static final class EnvoyReaderServiceStub
-      extends io.grpc.stub.AbstractAsyncStub<EnvoyReaderServiceStub> {
-    private EnvoyReaderServiceStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+  /**
+   */
+  public static final class EnvoyReaderServiceStub extends io.grpc.stub.AbstractAsyncStub<EnvoyReaderServiceStub> {
+    private EnvoyReaderServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
     }
 
@@ -76,9 +96,9 @@ public final class EnvoyReaderServiceGrpc {
     }
   }
 
-  /** */
-  public static final class EnvoyReaderServiceBlockingStub
-      extends io.grpc.stub.AbstractBlockingStub<EnvoyReaderServiceBlockingStub> {
+  /**
+   */
+  public static final class EnvoyReaderServiceBlockingStub extends io.grpc.stub.AbstractBlockingStub<EnvoyReaderServiceBlockingStub> {
     private EnvoyReaderServiceBlockingStub(
         io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
@@ -91,10 +111,11 @@ public final class EnvoyReaderServiceGrpc {
     }
   }
 
-  /** */
-  public static final class EnvoyReaderServiceFutureStub
-      extends io.grpc.stub.AbstractFutureStub<EnvoyReaderServiceFutureStub> {
-    private EnvoyReaderServiceFutureStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+  /**
+   */
+  public static final class EnvoyReaderServiceFutureStub extends io.grpc.stub.AbstractFutureStub<EnvoyReaderServiceFutureStub> {
+    private EnvoyReaderServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
     }
 
@@ -105,11 +126,12 @@ public final class EnvoyReaderServiceGrpc {
     }
   }
 
-  private static final class MethodHandlers<Req, Resp>
-      implements io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
-          io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
-          io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
-          io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
     private final EnvoyReaderServiceImplBase serviceImpl;
     private final int methodId;
 
@@ -138,9 +160,8 @@ public final class EnvoyReaderServiceGrpc {
     }
   }
 
-  private abstract static class EnvoyReaderServiceBaseDescriptorSupplier
-      implements io.grpc.protobuf.ProtoFileDescriptorSupplier,
-          io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+  private static abstract class EnvoyReaderServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
     EnvoyReaderServiceBaseDescriptorSupplier() {}
 
     @java.lang.Override
@@ -182,11 +203,9 @@ public final class EnvoyReaderServiceGrpc {
       synchronized (EnvoyReaderServiceGrpc.class) {
         result = serviceDescriptor;
         if (result == null) {
-          serviceDescriptor =
-              result =
-                  io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
-                      .setSchemaDescriptor(new EnvoyReaderServiceFileDescriptorSupplier())
-                      .build();
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new EnvoyReaderServiceFileDescriptorSupplier())
+              .build();
         }
       }
     }
