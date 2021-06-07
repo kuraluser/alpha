f467362b3017c0ec2aa3dbaf0cc7d1dad108bd9d
noora-ao <noora.t@alphaori.sg>
2021-06-07 14:08:27 +0530

    resolving duplicate issue

--- api/cp-dss-api/loadable-study/src/main/java/com/cpdss/loadablestudy/repository/VoyageRepository.java
+++ api/cp-dss-api/loadable-study/src/main/java/com/cpdss/loadablestudy/repository/VoyageRepository.java
@@ -52,7 +52,7 @@ public interface VoyageRepository
           "and v.vessel_xid = :vesselXId\n" +
           "and v.voyage_status notnull\n" +
           "order by v.created_date_time desc)\n" +
-          "select * from list1 union all select * from list2;", nativeQuery = true)
+          "select * from list1 union all select * from list2 where voyage_status != 3;", nativeQuery = true)
   public List<Voyage> findByIsActiveAndVesselXIdOrderByVoyageStatusDescAndLastModifiedDateTimeDesc(
           boolean isActive, Long vesselXId);
   @Query("select V from Voyage V WHERE V.voyageStatus.id =?1 AND V.isActive=?2")
