249d9c04cead7e866ee3b6d21c988b72a27613be
lincyignus <lincy.g@alphaori.sg>
2021-06-16 09:53:45 +0530

    DSS-2613 comm intgrtn

--- api/cp-dss-api/loadable-study/src/main/java/com/cpdss/loadablestudy/service/LoadableStudySchedulerService.java
+++ api/cp-dss-api/loadable-study/src/main/java/com/cpdss/loadablestudy/service/LoadableStudySchedulerService.java
@@ -2,8 +2,7 @@
 package com.cpdss.loadablestudy.service;
 
 import com.cpdss.common.exception.GenericServiceException;
-import com.cpdss.common.generated.VesselInfo;
-import com.cpdss.common.generated.VesselInfoServiceGrpc;
+import com.cpdss.common.generated.*;
 import com.cpdss.common.rest.CommonErrorCodes;
 import com.cpdss.common.scheduler.ScheduledTaskProperties;
 import com.cpdss.common.scheduler.ScheduledTaskRequest;
@@ -31,12 +30,16 @@ public class LoadableStudySchedulerService {
   @GrpcClient("vesselInfoService")
   private VesselInfoServiceGrpc.VesselInfoServiceBlockingStub vesselInfoGrpcService;
 
+  @GrpcClient("taskManagerService")
+  private TaskManagerServiceGrpc.TaskManagerServiceBlockingStub taskManagerGrpcService;
+
   @Value("${cpdss.build.env}")
   private String environment;
 
   // @PostConstruct
   public void init() {
     List<SchedulerRequest> vesselList = getAllVessel();
+    List<String> scheduledTasks = getScheduledTasks();
     vesselList.forEach(
         vesssel -> {
           new Thread(
@@ -47,8 +50,9 @@ public class LoadableStudySchedulerService {
                       LocalDateTime dateTime = LocalDateTime.now();
                       LocalDateTime endDateTime = dateTime.plus(Duration.ofDays(2));
                       ScheduledTaskProperties properties = new ScheduledTaskProperties();
-                      properties.setTaskName("DOWNLOAD_RESULT_SHIP" + vesssel.getVesselId());
-                      properties.setTaskFrequency(1);
+                      properties.setTaskName(
+                          "DOWNLOAD_RESULT_" + environment + "_" + vesssel.getVesselId());
+                      properties.setTaskFrequency(30);
                       properties.setTaskType(ScheduledTaskProperties.TaskTypeEnum.ASYNC);
                       properties.setTaskStartDate(dateTime.toLocalDate());
                       properties.setTaskStartTime(dateTime.toLocalTime());
@@ -63,7 +67,9 @@ public class LoadableStudySchedulerService {
                       requestParam.put("ClientId", vesssel.getVesselName());
                       requestParam.put("ShipId", String.valueOf(vesssel.getShipId()));
                       properties.setTaskReqParam(requestParam);
-                      scheduledTaskRequest.createScheduledTaskRequest(properties);
+                      if (scheduledTasks != null
+                          && !scheduledTasks.contains(properties.getTaskName()))
+                        scheduledTaskRequest.createScheduledTaskRequest(properties);
 
                     } catch (InterruptedException | GenericServiceException e) {
                       e.printStackTrace();
@@ -96,8 +102,32 @@ public class LoadableStudySchedulerService {
                       new SchedulerRequest(vessel.getName(), vessel.getId(), vessel.getImoNumber()))
               .collect(Collectors.toList());
     } catch (GenericServiceException e) {
-      e.printStackTrace();
+      log.error("GenericServiceException when generating pattern", e);
     }
     return requestList;
+  }
+
+  private List<String> getScheduledTasks() {
+    List<String> scheduledTasks = null;
+    TaskManager.SchedulededTaskRequest.Builder request =
+        TaskManager.SchedulededTaskRequest.newBuilder();
+    request.setJobGroup("CPDSS");
+    try {
+      TaskManager.SchedulededTaskReply taskReply =
+          this.taskManagerGrpcService.getScheduledTasks(request.build());
+      if (!LoadableStudiesConstants.SUCCESS.equalsIgnoreCase(
+          taskReply.getResponseStatus().getStatus())) {
+
+        throw new GenericServiceException(
+            "Error in calling task Manager service",
+            CommonErrorCodes.E_GEN_INTERNAL_ERR,
+            HttpStatusCode.INTERNAL_SERVER_ERROR);
+      }
+      scheduledTasks = taskReply.getTaskNameList();
+
+    } catch (GenericServiceException e) {
+      log.error("GenericServiceException when generating pattern", e);
+    }
+    return scheduledTasks;
   }
 }
