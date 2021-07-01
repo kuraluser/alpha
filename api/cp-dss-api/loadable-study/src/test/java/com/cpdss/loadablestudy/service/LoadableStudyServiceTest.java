9b5d1ce1dbf51eb5bcd57b8cd6fa30429d55e319
jeringeorgealpha <71864675+jeringeorgealpha@users.noreply.github.com>
2021-07-01 19:23:37 +0530

    build issue fixing (#1311)
    Co-authored-by: Jerin George <jerin.g@thinkpalm.com>

--- api/cp-dss-api/loadable-study/src/test/java/com/cpdss/loadablestudy/service/LoadableStudyServiceTest.java
+++ api/cp-dss-api/loadable-study/src/test/java/com/cpdss/loadablestudy/service/LoadableStudyServiceTest.java
@@ -3605,6 +3605,11 @@ class LoadableStudyServiceTest {
                 any(LocalDateTime.class), anyLong(), anyBoolean(), any()))
     .thenReturn(voyage);*/
     Mockito.when(
+            this.voyageRepository
+                .findFirstByVesselXIdAndIsActiveAndVoyageStatusOrderByLastModifiedDateDesc(
+                    anyLong(), anyBoolean(), any()))
+        .thenReturn(voyage);
+    Mockito.when(
             this.voyageHistoryRepository.findFirstByVoyageOrderByPortOrderDesc(any(Voyage.class)))
         .thenReturn((VoyageHistory) createDummyObject(VoyageHistory.class));
     Mockito.when(this.cargoHistoryRepository.findCargoHistory(anyLong(), anyLong()))
