3096186f81dbdf1f79d00c169416a35046652399
arunjohnsonthinkpalm <arun.j@thinkpalm.com>
2021-07-06 10:07:38 +0530

    proto file changes

--- api/cp-dss-api/common/src/main/java/com/cpdss/common/generated/dischargestudy/DischargeStudyModel.java
+++ api/cp-dss-api/common/src/main/java/com/cpdss/common/generated/dischargestudy/DischargeStudyModel.java
@@ -19,16 +19,19 @@ public final class DischargeStudyModel {
   static {
     java.lang.String[] descriptorData = {
       "\n*dischargestudy/discharge_study_model.p"
-          + "roto\032\014common.protoB-\n)com.cpdss.common.g"
-          + "enerated.dischargestudyP\000b\006proto3"
+          + "roto\032\014common.proto\032\024loadable_study.proto"
+          + "B-\n)com.cpdss.common.generated.discharge"
+          + "studyP\000b\006proto3"
     };
     descriptor =
         com.google.protobuf.Descriptors.FileDescriptor.internalBuildGeneratedFileFrom(
             descriptorData,
             new com.google.protobuf.Descriptors.FileDescriptor[] {
               com.cpdss.common.generated.Common.getDescriptor(),
+              com.cpdss.common.generated.LoadableStudy.getDescriptor(),
             });
     com.cpdss.common.generated.Common.getDescriptor();
+    com.cpdss.common.generated.LoadableStudy.getDescriptor();
   }
 
   // @@protoc_insertion_point(outer_class_scope)
--- api/cp-dss-api/common/src/main/java/com/cpdss/common/generated/dischargestudy/DischargeStudyServiceOuterClass.java
+++ api/cp-dss-api/common/src/main/java/com/cpdss/common/generated/dischargestudy/DischargeStudyServiceOuterClass.java
@@ -19,28 +19,23 @@ public final class DischargeStudyServiceOuterClass {
   static {
     java.lang.String[] descriptorData = {
       "\n,dischargestudy/discharge_study_service"
-          + ".proto\032\014common.proto\032\024loadable_study.pro"
-          + "to\032&loading_plan/loading_plan_models.pro"
-          + "to\032*dischargestudy/discharge_study_model"
-          + ".proto2\200\001\n\025DischargeStudyService\022g\n\031getD"
-          + "ischargeStudyByVoyage\022$.LoadingInformati"
-          + "onSynopticalRequest\032\".LoadingInformation"
-          + "SynopticalReply\"\000B-\n)com.cpdss.common.ge"
-          + "nerated.dischargestudyP\000b\006proto3"
+          + ".proto\032\014common.proto\032&loading_plan/loadi"
+          + "ng_plan_models.proto2\200\001\n\025DischargeStudyS"
+          + "ervice\022g\n\031getDischargeStudyByVoyage\022$.Lo"
+          + "adingInformationSynopticalRequest\032\".Load"
+          + "ingInformationSynopticalReply\"\000B-\n)com.c"
+          + "pdss.common.generated.dischargestudyP\000b\006"
+          + "proto3"
     };
     descriptor =
         com.google.protobuf.Descriptors.FileDescriptor.internalBuildGeneratedFileFrom(
             descriptorData,
             new com.google.protobuf.Descriptors.FileDescriptor[] {
               com.cpdss.common.generated.Common.getDescriptor(),
-              com.cpdss.common.generated.LoadableStudy.getDescriptor(),
               com.cpdss.common.generated.loading_plan.LoadingPlanModels.getDescriptor(),
-              com.cpdss.common.generated.dischargestudy.DischargeStudyModel.getDescriptor(),
             });
     com.cpdss.common.generated.Common.getDescriptor();
-    com.cpdss.common.generated.LoadableStudy.getDescriptor();
     com.cpdss.common.generated.loading_plan.LoadingPlanModels.getDescriptor();
-    com.cpdss.common.generated.dischargestudy.DischargeStudyModel.getDescriptor();
   }
 
   // @@protoc_insertion_point(outer_class_scope)
