557aaf82975adba01d09c24397c0d4372d450302
pranavkrisha97 <pranav.k@alphaori.sg>
2021-07-29 20:46:11 +0530

    reverted hasLoadicator change

--- api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/domain/LoadablePlanRequest.java
+++ api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/domain/LoadablePlanRequest.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 public class LoadablePlanRequest {
   private String processId;
-  private Boolean hasLodicator;
+  private boolean hasLodicator;
   private List<LoadablePlanDetails> loadablePlanDetails;
   private List<AlgoError> errors;
   private Long loadablePatternId;
