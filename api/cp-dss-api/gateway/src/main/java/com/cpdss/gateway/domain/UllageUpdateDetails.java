794b2edf3fd353f7a6b52ad72f446241c10347c4
ravi89iamrc <ravichandran.r@alphaori.sg>
2021-08-17 03:00:51 +0530

    Update Ullage API
    Update Ullage API

---
+++ api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/domain/UllageUpdateDetails.java
@@ -0,0 +1,32 @@
+/* Licensed at AlphaOri Technologies */
+package com.cpdss.gateway.domain;
+
+import java.math.BigDecimal;
+import lombok.Data;
+
+/** @author ravi.r */
+@Data
+public class UllageUpdateDetails {
+
+  private Long id;
+
+  private BigDecimal loadingInformationId;
+
+  private BigDecimal tankId;
+
+  private BigDecimal temperature;
+
+  private BigDecimal correctedUllage;
+
+  private BigDecimal correctionFactor;
+
+  private BigDecimal quantity;
+
+  private BigDecimal observedM3;
+
+  private BigDecimal fillingRatio;
+
+  private BigDecimal api;
+
+  private BigDecimal ullage;
+}
