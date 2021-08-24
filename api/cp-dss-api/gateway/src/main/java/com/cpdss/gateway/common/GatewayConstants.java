b3b3e1a9e817ce29aa4b2de2d89a8de2b8c7c012
sanalkumarkk92 <86705660+sanalkumarkk92@users.noreply.github.com>
2021-08-24 17:54:43 +0530

    Spotless applied

--- api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/common/GatewayConstants.java
+++ api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/common/GatewayConstants.java
@@ -27,4 +27,18 @@ public class GatewayConstants {
           (long) VesselInfo.VesselPumpTypes.BALLAST_PUMP_VALUE,
           (long) VesselInfo.VesselPumpTypes.GS_PUMP_VALUE,
           (long) VesselInfo.VesselPumpTypes.IG_PUMP_VALUE);
+
+  public static final String STATUS_ACTIVE = "ACTIVE";
+  public static final String STATUS_CONFIRMED = "CONFIRMED";
+  public static final String STATUS_CLOSE = "CLOSED";
+
+  public static final String OPERATION_TYPE_ARR = "ARR";
+  public static final String OPERATION_TYPE_DEP = "DEP";
+
+  public static final String DATE_FORMAT = "dd-MM-yyyy HH:mm";
+
+  public static final String LOADABLE_STUDY_SAVE_REQUEST = "LS_SAVE";
+  public static final String DISCHARGE_STUDY_SAVE_REQUEST = "DS_SAVE";
+  public static final String LOADABLE_STUDY = "LS";
+  public static final String DICHARGE_STUDY = "DS";
 }
