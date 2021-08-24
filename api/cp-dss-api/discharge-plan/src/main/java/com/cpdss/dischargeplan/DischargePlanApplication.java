4bc208f91f2b9deba0a9a8ba6b8da8d405bdbbc2
johnsoorajxavier <79301295+johnsoorajxavier@users.noreply.github.com>
2021-08-24 10:38:30 +0530

    New Microservice for Discharge study DSS-3821 (#1753)
    * DSS-3821: New Micro Service Config
    1. discharge plan service and config
    type: feature
    
    * DSS-3821: New Micro Service Config
    1. proto file/ service/ common package added
    type: feature
    
    * DSS-3821: Line content data type issue fix
    type: feature

---
+++ api/cp-dss-api/discharge-plan/src/main/java/com/cpdss/dischargeplan/DischargePlanApplication.java
@@ -0,0 +1,13 @@
+/* Licensed at AlphaOri Technologies */
+package com.cpdss.dischargeplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DischargePlanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DischargePlanApplication.class, args);
+  }
+}
