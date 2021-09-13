0f93782ba069682f279a79e6ec7daab73030d8e5
afnan-ashraf <89001907+afnan-ashraf@users.noreply.github.com>
2021-09-13 11:09:24 +0530

    PR Pipeline Implementation (#1927)
    * Added Yaml pipeline script for angular services
    
    * Added communication stack files
    
    * Update CI yaml script
    
    * Update cpdss-ci-pr-comm-angular-yaml.yml for Azure Pipelines
    
    * Added rollback settings to envoy client and server

---
+++ devops/pipeline-scripts/docker-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo systemctl status docker
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+cd /etc/systemd/system/
+
+sudo chmod -R 777 docker.service.d
+
+sudo touch /etc/systemd/system/docker.service.d/options.conf
+sudo chmod -R 777 options.conf
+sudo cat >/etc/systemd/system/docker.service.d/options.conf <<EOL
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H unix:// -H tcp://0.0.0.0:2375
+EOL
+
+sudo systemctl daemon-reload
+
+sudo systemctl restart docker
+
+sudo systemctl status docker
