bcfddcc458e6656adef340e3dcde75a74b4d9491
gokulprasannan <75689197+gokulprasannan@users.noreply.github.com>
2021-10-07 19:19:18 +0530

    Feature bug simulator algo response change (#2197)
    * Checking empty string instead of null
    
    * Committing for algo response change
    -  added quantityM3 and loadablePlanRoBDetails
    
    * Committing to incorporate changed algo output.

---
+++ api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/domain/simulator/LoadablePlanRobDetails.java
@@ -0,0 +1,14 @@
+package com.cpdss.gateway.domain.simulator;
+
+import lombok.Data;
+
+/** @Author gokul.p */
+@Data
+public class LoadablePlanRobDetails {
+    private Double quantity;
+    private Double quantityM3;
+    private Double sg;
+    private Long tankId;
+    private String tankName;
+    private String tankShortName;
+}
