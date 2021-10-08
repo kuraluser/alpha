bcac24a4c2dc8cd1acf8a8bfc8e4bda11cb23ee5
rishi-tp-alphaori <79296901+rishi-tp-alphaori@users.noreply.github.com>
2021-10-08 09:15:29 +0530

    DSS-4000 : Issue with simulator options for confirmed LS (#2200)
    * DSS-4000 : Issue with simulator options for confirmed LS
    
    * removed unwanted code
    
    * function comment updated
    
    Co-authored-by: Rishiraj CR <rishiraj.cr@Thinkpalm.com>

---
+++ web/cpdss-portal/apps/cpdss/src/assets/external/simulator-js/load.js
@@ -0,0 +1,124 @@
+function _0x3453(_0x2d58d3, _0x4e5734) {
+    var _0x500ff9 = _0x500f();
+    return (
+        (_0x3453 = function (_0x34538c, _0x5128f1) {
+            _0x34538c = _0x34538c - 0x116;
+            var _0x192818 = _0x500ff9[_0x34538c];
+            return _0x192818;
+        }),
+        _0x3453(_0x2d58d3, _0x4e5734)
+    );
+}
+function _0x500f() {
+    var _0x370edc = [
+        "method",
+        "open",
+        "stringify",
+        "780230LbcEeU",
+        "error",
+        "source",
+        "\x20Error\x20Message\x0a\x20",
+        "\x20is\x20not\x20valid",
+        "721614mPquMV",
+        "199CVJNuW",
+        "Window\x20",
+        "943984bIKzYZ",
+        "Window\x20Id\x20",
+        "postMessage",
+        "12908FCIaJu",
+        "origin",
+        "message",
+        "load",
+        "winId",
+        "addEventListener",
+        "toString",
+        "onLoad",
+        "replace",
+        "menubar=no,location=no,resizable=yes,scrollbars=yes,status=no",
+        "153lnrVMw",
+        "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx",
+        "args",
+        "parse",
+        "2cgQaTQ",
+        "data",
+        "877585dQCkiH",
+        "https://ec2-13-250-56-222.ap-southeast-1.compute.amazonaws.com",
+        "352CozGTU",
+        "onLoadData",
+        "1994760jijEtq",
+        "_blank",
+    ];
+    _0x500f = function () {
+        return _0x370edc;
+    };
+    return _0x500f();
+}
+(function (_0x2a3d08, _0x3c377e) {
+    var _0x253dab = _0x3453,
+        _0x479e61 = _0x2a3d08();
+    while (!![]) {
+        try {
+            var _0x17b593 =
+                parseInt(_0x253dab(0x12f)) / 0x1 +
+                (-parseInt(_0x253dab(0x11e)) / 0x2) * (parseInt(_0x253dab(0x12e)) / 0x3) +
+                -parseInt(_0x253dab(0x131)) / 0x4 +
+                -parseInt(_0x253dab(0x120)) / 0x5 +
+                -parseInt(_0x253dab(0x124)) / 0x6 +
+                (-parseInt(_0x253dab(0x134)) / 0x7) * (parseInt(_0x253dab(0x122)) / 0x8) +
+                (-parseInt(_0x253dab(0x11a)) / 0x9) * (-parseInt(_0x253dab(0x129)) / 0xa);
+            if (_0x17b593 === _0x3c377e) break;
+            else _0x479e61["push"](_0x479e61["shift"]());
+        } catch (_0x5197b8) {
+            _0x479e61["push"](_0x479e61["shift"]());
+        }
+    }
+})(_0x500f, 0x3fb4f),
+    (function (_0x210fd5) {
+        var _0x4d4f96 = _0x3453,
+            _0xf86277 = {},
+            _0x4b12c0 = ![],
+            _0x13d005 = _0x4d4f96(0x121);
+        function _0x451ca8() {
+            var _0x32cc81 = _0x4d4f96;
+            return _0x32cc81(0x11b)[_0x32cc81(0x118)](/[xy]/g, function (_0xc3966e) {
+                var _0x516385 = _0x32cc81,
+                    _0x3f1472 = (Math["random"]() * 0x10) | 0x0,
+                    _0x32aa00 = _0xc3966e == "x" ? _0x3f1472 : (_0x3f1472 & 0x3) | 0x8;
+                return _0x32aa00[_0x516385(0x116)](0x10);
+            });
+        }
+        function _0x294f98(_0x26e7a2) {
+            var _0x2c2433 = _0x4d4f96;
+            if (_0x26e7a2["origin"] != _0x13d005) return;
+            var _0xeaca2 = _0x26e7a2[_0x2c2433(0x11f)];
+            try {
+                _0xeaca2 = JSON[_0x2c2433(0x11d)](_0xeaca2);
+                var _0x33b84c = _0xeaca2[_0x2c2433(0x138)];
+                if (_0xf86277["hasOwnProperty"](_0x33b84c))
+                    switch (_0xeaca2[_0x2c2433(0x126)]) {
+                        case _0x2c2433(0x117): {
+                            var _0x315c1a = _0xf86277[_0x33b84c][_0x2c2433(0x123)];
+                            _0x26e7a2[_0x2c2433(0x12b)][_0x2c2433(0x133)](JSON[_0x2c2433(0x128)](_0x315c1a), _0x26e7a2[_0x2c2433(0x135)]);
+                            break;
+                        }
+                        case "onError": {
+                            var _0xeaca2 = _0xeaca2[_0x2c2433(0x11c)]["data"];
+                            throw new Error(_0x2c2433(0x130) + _0x33b84c + _0x2c2433(0x12c) + JSON[_0x2c2433(0x128)](_0xeaca2, null, 0x2));
+                            break;
+                        }
+                    }
+                else throw new Error(_0x2c2433(0x132) + _0x33b84c + _0x2c2433(0x12d));
+            } catch (_0x484ec2) {
+                console[_0x2c2433(0x12a)](_0x484ec2[_0x2c2433(0x136)]);
+            }
+        }
+        function _0x4fd414(_0x3e27f6, _0x5f3afa, _0x194b55, _0x1d9450, _0x223de1, _0x268c2f, _0x2351bd) {
+            var _0x4b1c29 = _0x4d4f96,
+                _0x511815 = { method: _0x4b1c29(0x137) + _0x2351bd, args: { ship: _0x3e27f6, json: _0x5f3afa, loadicatorJson: _0x194b55, path: _0x1d9450, user: _0x223de1, role: _0x268c2f } },
+                _0x14d66a = _0x451ca8(),
+                _0x366d77 = _0x13d005 + "/sims/cargo/embedded.html?winId=" + _0x14d66a,
+                _0x40a9f8 = window[_0x4b1c29(0x127)](_0x366d77, _0x4b1c29(0x125), _0x4b1c29(0x119));
+            return (_0xf86277[_0x14d66a] = { win: _0x40a9f8, onLoadData: _0x511815 }), !_0x4b12c0 && ((_0x4b12c0 = !![]), window[_0x4b1c29(0x139)]("message", _0x294f98)), [];
+        }
+        _0x210fd5[_0x4d4f96(0x137)] = _0x4fd414;
+    })(window);
