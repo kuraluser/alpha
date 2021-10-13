c403fd668373d2b8a6ee19b944dc9e8a3689372a
pranavkrisha97 <pranav.k@alphaori.sg>
2021-10-13 10:01:04 +0530

    added eduction to loading plan

---
+++ api/cp-dss-api/loading-plan/src/main/java/com/cpdss/loadingplan/repository/EductionOperationRepository.java
@@ -0,0 +1,20 @@
+/* Licensed at AlphaOri Technologies */
+package com.cpdss.loadingplan.repository;
+
+import com.cpdss.common.springdata.CommonCrudRepository;
+import com.cpdss.loadingplan.entity.EductionOperation;
+import com.cpdss.loadingplan.entity.LoadingSequence;
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/** @author pranav.k */
+@Repository
+public interface EductionOperationRepository extends CommonCrudRepository<EductionOperation, Long> {
+
+  @Modifying
+  @Transactional
+  @Query(value = "UPDATE EductionOperation SET isActive = false WHERE loadingSequence = ?1")
+  public void deleteByLoadingSequence(LoadingSequence loadingSequence);
+}
