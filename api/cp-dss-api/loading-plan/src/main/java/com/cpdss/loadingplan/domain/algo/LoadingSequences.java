3e6bef8c48900b183078b115bb1a802a10563f7c
pranavkrisha97 <pranav.k@alphaori.sg>
2021-10-22 10:23:00 +0530

    fixed loading plan input population issue

--- api/cp-dss-api/loading-plan/src/main/java/com/cpdss/loadingplan/domain/algo/LoadingSequences.java
+++ api/cp-dss-api/loading-plan/src/main/java/com/cpdss/loadingplan/domain/algo/LoadingSequences.java
@@ -1,10 +1,12 @@
 /* Licensed at AlphaOri Technologies */
 package com.cpdss.loadingplan.domain.algo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LoadingSequences {
 
   private List<ReasonForDelay> reasonForDelays;
