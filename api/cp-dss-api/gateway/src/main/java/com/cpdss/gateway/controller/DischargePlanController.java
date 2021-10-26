4119e3e9d52f90c6cccf253a399a18c4d8f3cb09
arunjohnsonthinkpalm <arun.j@thinkpalm.com>
2021-10-26 17:55:21 +0530

    confirm pla API isue

--- api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/controller/DischargePlanController.java
+++ api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/controller/DischargePlanController.java
@@ -576,7 +576,7 @@ public class DischargePlanController {
    * @return
    * @throws CommonRestException
    */
-  @PostMapping(value = "discharge-studies/{dischargeStudyId}/confirm-plan/{dischargePatternId}")
+  @PostMapping(value = "/vessels/{vesselId}/voyages/{voyageId}/discharge-studies/{dischargeStudyId}/confirm-plan/{dischargePatternId}")
   public CommonResponse confirmPlan(
       @PathVariable Long dischargeStudyId,
       @PathVariable Long dischargePatternId,
