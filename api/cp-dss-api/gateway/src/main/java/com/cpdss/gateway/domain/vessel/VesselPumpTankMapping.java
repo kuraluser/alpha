9cbc21aea76165e72192dcad55c32a4b5ef807e5
sreemanikandan.k <sreemanikandan.k@alphaori.sg>
2021-11-09 00:19:00 +0530

    DSS-4567: Add CargoTank and CargoPump Mapping in Vessel Details Json.
    1. Made changes to add CargoTank and CargoPump.
    2. Added VesselPumpTankMappingRepository in Vessel microservice.
    3. Added VesselPumpTankMapping domain in gateway.
    
    Type: Enhancement

---
+++ api/cp-dss-api/gateway/src/main/java/com/cpdss/gateway/domain/vessel/VesselPumpTankMapping.java
@@ -0,0 +1,13 @@
+/* Licensed at AlphaOri Technologies */
+package com.cpdss.gateway.domain.vessel;
+
+import com.cpdss.gateway.domain.VesselTank;
+import lombok.Data;
+
+@Data
+public class VesselPumpTankMapping {
+
+  private Integer vesselXid;
+  private VesselPump vesselPump;
+  private VesselTank vesselTank;
+}
