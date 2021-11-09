612bb3c4c84f244d5a84f314774e8416ed362cd2
Selvy Thomas <selvy.t@alphaori.sg>
2021-11-09 17:29:00 +0530

    communication LoadingPlan changes.

--- api/cp-dss-api/loading-plan/src/main/java/com/cpdss/loadingplan/repository/LoadingInformationCommunicationRepository.java
+++
@@ -1,8 +0,0 @@
-/* Licensed at AlphaOri Technologies */
-package com.cpdss.loadingplan.repository;
-
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional(propagation = Propagation.REQUIRES_NEW)
-public interface LoadingInformationCommunicationRepository extends LoadingInformationRepository {}
