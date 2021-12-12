fbd5f99313ecb3037ee2ce27c5095d7816f86217
sreemanikandan.k <sreemanikandan.k@alphaori.sg>
2021-12-12 20:53:17 +0530

    DSS-4963: Delete API for cargo master listing
    1. Added API.
    2. Added Service classes.
    3. Added GRPC method.
    4. Added Repo methods.
    
    Type: Development

--- api/cp-dss-api/common/src/main/java/com/cpdss/common/generated/CargoInfoServiceGrpc.java
+++ api/cp-dss-api/common/src/main/java/com/cpdss/common/generated/CargoInfoServiceGrpc.java
@@ -308,6 +308,53 @@ public final class CargoInfoServiceGrpc {
     return getGetCargoInfoDetailedByIdMethod;
   }
 
+  private static volatile io.grpc.MethodDescriptor<
+          com.cpdss.common.generated.CargoInfo.CargoRequest,
+          com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>
+      getDeleteCargoByIdMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "DeleteCargoById",
+      requestType = com.cpdss.common.generated.CargoInfo.CargoRequest.class,
+      responseType = com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<
+          com.cpdss.common.generated.CargoInfo.CargoRequest,
+          com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>
+      getDeleteCargoByIdMethod() {
+    io.grpc.MethodDescriptor<
+            com.cpdss.common.generated.CargoInfo.CargoRequest,
+            com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>
+        getDeleteCargoByIdMethod;
+    if ((getDeleteCargoByIdMethod = CargoInfoServiceGrpc.getDeleteCargoByIdMethod) == null) {
+      synchronized (CargoInfoServiceGrpc.class) {
+        if ((getDeleteCargoByIdMethod = CargoInfoServiceGrpc.getDeleteCargoByIdMethod) == null) {
+          CargoInfoServiceGrpc.getDeleteCargoByIdMethod =
+              getDeleteCargoByIdMethod =
+                  io.grpc.MethodDescriptor
+                      .<com.cpdss.common.generated.CargoInfo.CargoRequest,
+                          com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>
+                          newBuilder()
+                      .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+                      .setFullMethodName(generateFullMethodName(SERVICE_NAME, "DeleteCargoById"))
+                      .setSampledToLocalTracing(true)
+                      .setRequestMarshaller(
+                          io.grpc.protobuf.ProtoUtils.marshaller(
+                              com.cpdss.common.generated.CargoInfo.CargoRequest
+                                  .getDefaultInstance()))
+                      .setResponseMarshaller(
+                          io.grpc.protobuf.ProtoUtils.marshaller(
+                              com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply
+                                  .getDefaultInstance()))
+                      .setSchemaDescriptor(
+                          new CargoInfoServiceMethodDescriptorSupplier("DeleteCargoById"))
+                      .build();
+        }
+      }
+    }
+    return getDeleteCargoByIdMethod;
+  }
+
   /** Creates a new async stub that supports all call types for the service */
   public static CargoInfoServiceStub newStub(io.grpc.Channel channel) {
     io.grpc.stub.AbstractStub.StubFactory<CargoInfoServiceStub> factory =
@@ -406,6 +453,15 @@ public final class CargoInfoServiceGrpc {
           getGetCargoInfoDetailedByIdMethod(), responseObserver);
     }
 
+    /** */
+    public void deleteCargoById(
+        com.cpdss.common.generated.CargoInfo.CargoRequest request,
+        io.grpc.stub.StreamObserver<com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>
+            responseObserver) {
+      io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(
+          getDeleteCargoByIdMethod(), responseObserver);
+    }
+
     @java.lang.Override
     public final io.grpc.ServerServiceDefinition bindService() {
       return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
@@ -451,6 +507,13 @@ public final class CargoInfoServiceGrpc {
                       com.cpdss.common.generated.CargoInfo.CargoRequest,
                       com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>(
                       this, METHODID_GET_CARGO_INFO_DETAILED_BY_ID)))
+          .addMethod(
+              getDeleteCargoByIdMethod(),
+              io.grpc.stub.ServerCalls.asyncUnaryCall(
+                  new MethodHandlers<
+                      com.cpdss.common.generated.CargoInfo.CargoRequest,
+                      com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>(
+                      this, METHODID_DELETE_CARGO_BY_ID)))
           .build();
     }
   }
@@ -532,6 +595,17 @@ public final class CargoInfoServiceGrpc {
           request,
           responseObserver);
     }
+
+    /** */
+    public void deleteCargoById(
+        com.cpdss.common.generated.CargoInfo.CargoRequest request,
+        io.grpc.stub.StreamObserver<com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>
+            responseObserver) {
+      io.grpc.stub.ClientCalls.asyncUnaryCall(
+          getChannel().newCall(getDeleteCargoByIdMethod(), getCallOptions()),
+          request,
+          responseObserver);
+    }
   }
 
   /** */
@@ -587,6 +661,13 @@ public final class CargoInfoServiceGrpc {
         com.cpdss.common.generated.CargoInfo.CargoRequest request) {
       return io.grpc.stub.ClientCalls.blockingUnaryCall(
           getChannel(), getGetCargoInfoDetailedByIdMethod(), getCallOptions(), request);
+    }
+
+    /** */
+    public com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply deleteCargoById(
+        com.cpdss.common.generated.CargoInfo.CargoRequest request) {
+      return io.grpc.stub.ClientCalls.blockingUnaryCall(
+          getChannel(), getDeleteCargoByIdMethod(), getCallOptions(), request);
     }
   }
 
@@ -650,6 +731,14 @@ public final class CargoInfoServiceGrpc {
       return io.grpc.stub.ClientCalls.futureUnaryCall(
           getChannel().newCall(getGetCargoInfoDetailedByIdMethod(), getCallOptions()), request);
     }
+
+    /** */
+    public com.google.common.util.concurrent.ListenableFuture<
+            com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>
+        deleteCargoById(com.cpdss.common.generated.CargoInfo.CargoRequest request) {
+      return io.grpc.stub.ClientCalls.futureUnaryCall(
+          getChannel().newCall(getDeleteCargoByIdMethod(), getCallOptions()), request);
+    }
   }
 
   private static final int METHODID_GET_CARGO_INFO = 0;
@@ -658,6 +747,7 @@ public final class CargoInfoServiceGrpc {
   private static final int METHODID_GET_CARGO_INFOS_BY_CARGO_IDS = 3;
   private static final int METHODID_GET_CARGO_INFO_DETAILED = 4;
   private static final int METHODID_GET_CARGO_INFO_DETAILED_BY_ID = 5;
+  private static final int METHODID_DELETE_CARGO_BY_ID = 6;
 
   private static final class MethodHandlers<Req, Resp>
       implements io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
@@ -708,6 +798,13 @@ public final class CargoInfoServiceGrpc {
           break;
         case METHODID_GET_CARGO_INFO_DETAILED_BY_ID:
           serviceImpl.getCargoInfoDetailedById(
+              (com.cpdss.common.generated.CargoInfo.CargoRequest) request,
+              (io.grpc.stub.StreamObserver<
+                      com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>)
+                  responseObserver);
+          break;
+        case METHODID_DELETE_CARGO_BY_ID:
+          serviceImpl.deleteCargoById(
               (com.cpdss.common.generated.CargoInfo.CargoRequest) request,
               (io.grpc.stub.StreamObserver<
                       com.cpdss.common.generated.CargoInfo.CargoByIdDetailedReply>)
@@ -783,6 +880,7 @@ public final class CargoInfoServiceGrpc {
                       .addMethod(getGetCargoInfosByCargoIdsMethod())
                       .addMethod(getGetCargoInfoDetailedMethod())
                       .addMethod(getGetCargoInfoDetailedByIdMethod())
+                      .addMethod(getDeleteCargoByIdMethod())
                       .build();
         }
       }
