e9fd1c03b9dd4165ee1e41d2b92f169088d160db
athulcp75 <98143400+athulcp75@users.noreply.github.com>
2022-01-27 14:24:37 +0530

    DSS-5523: save the LS with file in new ls creation pop up not working (#3261)
    * DSS-5523: save the LS with file in new ls creation pop up is not working
    1.allowed file extension changed
    2. Error message updated
    
    * DSS-5523: save the LS with file in new ls creation pop up not working
    1. allowed extension updated
    2 changed error message
    
    Co-authored-by: THINKPALM\athul.cp <athul.cp@thinkpalm.com>

--- web/cpdss-portal/apps/cpdss/src/app/business/core/components/new-loadable-study-popup/new-loadable-study-popup.component.ts
+++ web/cpdss-portal/apps/cpdss/src/app/business/core/components/new-loadable-study-popup/new-loadable-study-popup.component.ts
@@ -234,7 +234,7 @@ export class NewLoadableStudyPopupComponent implements OnInit {
     let uploadFile = [];
     const sizeErrorFiles = [];
     const uploadedFileVar = this.fileUploadVariable.nativeElement.files;
-    const extensions = ["docx", "pdf", "txt", "jpg", "jpeg", "png", "eml", "msg"];
+    const extensions = ['docx','pdf','txt','csv','xlsx'];
     if (this.uploadedFiles.length < 5) {
       for (let i = 0; i < uploadedFileVar.length; i++) {
         const fileExtension = uploadedFileVar[i].name.substr((uploadedFileVar[i].name.lastIndexOf('.') + 1));
